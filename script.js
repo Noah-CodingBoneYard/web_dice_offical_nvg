259b84305eb74a03b51bc1e0e0de5c7a94f3c6d7
Noah-CodingBoneYard <Noahavangorp@lewisu.edu>
2024-10-05 23:16:23 -0500

    math is mathin

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-function rollDice() {
+/*function rollDice() {
     for (let i = 1; i <= 5; i++) {
         const diceValue = Math.floor(Math.random() * 6) + 1;
         document.getElementById(`dice${i}`).value = diceValue;
     }
     document.getElementById('rollButton').focus();
+}*/
+
+async function rollDice() {
+    try {
+        const response = await fetch('https://web-dice-roller-nodejs-nvg-g7fefwetc3dqg5ap.centralus-01.azurewebsites.net/d6');
+        const data = await response.json();
+        for (let i = 1; i <= 5; i++) {
+            const dice = document.getElementById(`dice${i}`);
+            dice.value = data.dice[i - 1];
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
